126303825f93397a28f8afeb2641a6820e12bf0e
awen970204-lgtm <awen970204@gmail.com>
2026-05-14 22:08:56 +0800

    Item

---
+++ ItemControl.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+public class ItemControl : MonoBehaviour
+{
+    [HideInInspector] public Item item;
+
+    [SerializeField] private Image itemPicter;
+    [SerializeField] private TMP_Text itemName;
+    
+    void Start()
+    {
+        if (item != null)
+        {
+            itemPicter.sprite = item.itemPicture;
+            itemName.text = item.itemName;
+        }
+    }
+}
---
+++ ScriptableObject/Item.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Item")]
+public class Item : ScriptableObject
+{
+    public string itemName;
+    public Sprite itemPicture;
+}
